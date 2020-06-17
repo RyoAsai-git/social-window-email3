9d16740c25928017fb045e0cacc6c15bb75ceefb
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-06-17 17:34:53 +0900

    fixed

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,5 @@
 class InquiryMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.inquiry_mailer.received_email.subject
-  #
   def received_email(user)
     @user = user
     mail to: @user.email,
