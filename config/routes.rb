11b14982ca1af2ae15aae0eddea810b66458bdf6
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-06-17 16:31:05 +0900

    root作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'inquiries/new'
+  root 'inquiries#new'
   get 'inquiries/confirm'
   get 'inquiries/thanks'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
